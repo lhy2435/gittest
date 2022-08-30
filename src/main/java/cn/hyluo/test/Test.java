660774d5733a63bc9e7abbf23f4bff893c586648
hyluo <hyluo@smit.com>
2022-08-30 17:28:34 +0800

    third commit

--- src/main/java/cn/hyluo/test/Test.java
+++ src/main/java/cn/hyluo/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world222222222");
+        System.out.println("hello world333333333");
     }
 }
